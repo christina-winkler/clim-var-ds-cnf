fix rotation of low res image TCW

data/era5_watercontent_dset.py
@@ -6,7 +6,7 @@ from typing import Tuple, Callable
 from skimage.transform import resize
 import torch
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 @dataclass
 class MinMaxScaler:
@@ -58,11 +58,11 @@ class ERA5WTCData(Dataset):
         x,y = self.inputs[idx], self.targets[idx]
 
         if self.s==2:
+            # import pdb; pdb.set_trace()
             x = np.zeros((1,1,y.shape[2]//2,y.shape[2]//2))
             x[0,0,...] = resize(y[0,0,...], (y.shape[2]//2, y.shape[3]//2), anti_aliasing=True)
             transf = transforms.ToTensor()
-            x = transf(x[0,...]).permute(1,0,2).unsqueeze(0).type(torch.FloatTensor)
-            # import pdb; pdb.set_trace()
+            x = transf(x[0,...]).permute(1,2,0).unsqueeze(0).type(torch.FloatTensor)
 
         if not self.testmode:
             return self.transform(y).squeeze(1), self.transform(x).squeeze(1)