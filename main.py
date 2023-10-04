fix type casting for scale computation, ow conflict with torch distribution package

main.py
@@ -73,8 +73,8 @@ def main(args):
         model = srflow.SRFlow((in_channels, args.height, args.width), args.filter_size, args.L, args.K,
                                args.bsz, args.s, args.nb, args.condch, args.nbits, args.noscale, args.noscaletest)
         if args.resume:
-            modelname = 'model_epoch_12_step_42250.tar'
-            modelpath = "/home/christina/Documents/clim-var-ds-cnf/runs/srflow_era5-TCW_2023_09_18_16_14_06_4x/model_checkpoints/{}".format(modelname)
+            modelname = 'model_epoch_1_step_53000.tar'
+            modelpath = "/home/christina/Documents/clim-var-ds-cnf/runs/srflow_era5-TCW_2023_10_02_18_59_01constraint2x/model_checkpoints/{}".format(modelname)
             ckpt = torch.load(modelpath)
             model.load_state_dict(ckpt['model_state_dict'])
 

models/transformations/modules_sr.py
@@ -327,12 +327,12 @@ class GaussianPrior(nn.Module):
     def split2d_prior(self, z, lr_feat_map):
         x = torch.cat((z, lr_feat_map), 1)
         h = self.conv(x)
-        mean, sigma = h[:, 0::2], nn.functional.softplus(h[:, 1::2])
+        mean, sigma = h[:, 0::2], nn.functional.softplus(h[:, 1::2].type(torch.DoubleTensor).cuda())
         return mean, sigma
 
     def final_prior(self, lr_feat_map):
         h = self.conv(lr_feat_map)
-        mean, sigma = h[:, 0::2], nn.functional.softplus(h[:, 1::2])
+        mean, sigma = h[:, 0::2], nn.functional.softplus(h[:, 1::2].type(torch.DoubleTensor).cuda())
         return mean, sigma
 
     def forward(self, x, lr_feat_map, eps=1.0, reverse=False, logpz=0, logdet=0,
@@ -354,7 +354,7 @@ class GaussianPrior(nn.Module):
             if not self.final:
                 mean, sigma = self.split2d_prior(x, lr_feat_map)
                 prior = torch.distributions.normal.Normal(loc=mean, scale=sigma*eps+0.00001)
-                z2 = prior.sample()
+                z2 = prior.sample().type(torch.FloatTensor).cuda()
                 logpz -= prior.log_prob(z2).sum(dim=[1,2,3])
                 z = torch.cat((x, z2), 1)
 
@@ -364,7 +364,7 @@ class GaussianPrior(nn.Module):
                 _, c, h, w = self.flow_var_shape
                 mean, sigma = self.final_prior(lr_feat_map)
                 prior = torch.distributions.normal.Normal(loc=mean, scale=sigma*eps+0.00001)
-                z = prior.sample()
+                z = prior.sample().type(torch.FloatTensor).cuda()
                 logpz -= prior.log_prob(z).sum(dim=[1,2,3])
                 # print("Test probs", torch.exp(logpz))
 

optimization/trainer_srflow.py
@@ -16,6 +16,7 @@ from tensorboardX import SummaryWriter
 from torch.optim.lr_scheduler import StepLR
 from models.architectures.conv_lstm import *
 from optimization.validation_srflow import validate
+from typing import Tuple, Callable
 
 import wandb
 os.environ["WANDB_SILENT"] = "true"
@@ -41,6 +42,12 @@ class SoftmaxConstraints(nn.Module):
         sum_y = self.pool(y)
         out = y*torch.kron(lr*1/sum_y, torch.ones((self.upsampling_factor,self.upsampling_factor)).to('cuda'))
         return out
+
+class MinMaxScaler:
+    def __call__(self, x, max_value, min_value):
+        values_range: Tuple[int, int] = (-1, 1)
+        x = (x - min_value) / (max_value - min_value)
+        return x * (values_range[1] - values_range[0]) + values_range[0]
 
 def inv_scaler(x, ref=None):
     min_value = ref.min()
@@ -72,7 +79,7 @@ def trainer(args, train_loader, valid_loader, model,
     state=None
 
     model.to(device)
-
+    scaler = MinMaxScaler()
     SC = SoftmaxConstraints(upsampling_factor=args.s)
     l1 = nn.L1Loss()
 
@@ -115,13 +122,15 @@ def trainer(args, train_loader, valid_loader, model,
             y_hat, logdet, logpz = model(xlr=x, reverse=True)
 
             # apply softmax constraint
-            out = SC(inv_scaler(y_hat,y), x_unnorm)
+            # out = SC(inv_scaler(y_hat,y), x_unnorm)
+            # scaler = MinMaxScaler()
+            # out_scaled = scaler(out, max_value=out.max(), min_value=out.min())
 
             writer.add_scalar("nll_train", nll.mean().item(), step)
             # wandb.log({"nll_train": nll.mean().item()}, step)
 
             # Compute gradients
-            loss = nll + l1(out,y_unnorm)
+            loss = nll  + l1(y_hat, y_unnorm[0,...])
             loss.mean().backward()
 
             # Update model parameters using calculated gradients
@@ -169,6 +178,7 @@ def trainer(args, train_loader, valid_loader, model,
 
                      # Super-Resolving low-res
                     y_hat, logdet, logpz = model(xlr=x, reverse=True)
+                    print(y_hat.max(), y_hat.min(), y.max(), y.min())
                     grid_y_hat = torchvision.utils.make_grid(y_hat[0:9, :, :, :].cpu(), nrow=3)
                     plt.figure()
                     plt.imshow(grid_y_hat.permute(1, 2, 0)[:,:,0], cmap=cmap)