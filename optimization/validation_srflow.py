add scaling and correct color

optimization/validation_srflow.py
@@ -29,7 +29,7 @@ def validate(model, val_loader, metric_dict, exp_name, logstep, args):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    cmap = 'viridis' if args.trainset == 'era5-TCW' else 'inferno'
+    cmap = 'viridis' # if args.trainset == 'era5-TCW' else 'inferno'
 
     nll_list=[]
     model.eval()

optimization/validation_srgan.py
@@ -39,7 +39,7 @@ def validate(discriminator, generator, val_loader, metric_dict, exp_name, logste
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    cmap = 'viridis' if args.trainset == 'era5-TCW' else 'inferno'
+    cmap = 'viridis' #if args.trainset == 'era5-TCW' else 'inferno'
 
     discriminator.eval()
     generator.eval()